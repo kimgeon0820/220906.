a9693240537f9d9482fa5b59166969a147755f05
건우 김 <dnjffud0820@naver.com>
2022-09-06 10:41:13 +0900

    별만들기..

--- star.c
+++ star.c
@@ -1,15 +1,47 @@
 #include <stdio.h>
-int main() {
 
+int main(void)
+{
 	int input = 0;
+	int i = 0, j = 0, k = 0, l = 0;
 
 	scanf_s("%d", &input);
 
-	for (int i = 0; i < input; i++)
+	for (i=input ; i > 0; i--)
 	{
-		printf("**");
+		for (j=i; j > 0; j--)
+		{
+			printf("*");
+		}
+		for (k = 2 * input - 2 * i; k > 0; k--)
+		{
+			printf(" ");
+		}
+		for (j = i; j > 0; j--)
+		{
+			printf("*");
+		}
+		printf("\n");
+		
+	}
+
+	for (i = 1; i <= input; i++)
+	{
+		for (j = i; j > 0; j--)
+		{
+			printf("*");
+		}
+		for (k = 2 * input - 2 * i; k > 0; k--)
+		{
+			printf(" ");
+		}
+		for (j = i; j > 0; j--)
+		{
+			printf("*");
+		}
+		printf("\n");
+
 	}
 
 	return 0;
-	system("pause");
 }
